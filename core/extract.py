425fd7369ea851499b941c46387b2ef2b11afbae
SahilAgarwal2004 <sahilagarwal19102004@gmail.com>
2026-09-17 01:11:39 +0530

    Branch First commit

--- core/extract.py
+++ core/extract.py
@@ -266,3 +266,6 @@ if __name__ == "__main__":
 
     print("\n── extract_resume(metadata=True) ───────────────────────")
     pprint.pprint(extract_resume(sample, metadata=True))
+
+
+    ### Sahil Initial
