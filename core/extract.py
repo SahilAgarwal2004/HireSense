79fc7578720db67473889f263694883bcea69f03
Dhruv Sapra <dhruvsapra448@gmail.com>
2026-09-17 01:05:18 +0530

    first branch

--- core/extract.py
+++ core/extract.py
@@ -266,3 +266,7 @@ if __name__ == "__main__":
 
     print("\n── extract_resume(metadata=True) ───────────────────────")
     pprint.pprint(extract_resume(sample, metadata=True))
+
+
+
+## dhruv's branch
